b66bbec7c62ae773648d599b3156094ce4bc78cf
Claudio Scandura <cscandura@equalexperts.com>
2018-09-11 09:40:09 +0100

    Calculate minimal path returns the minimal path as a sequence of nodes as well as the sum of all its the nodes for any valid triangle.

--- src/main/scala/com/suprnation/MinimalTrianglePath.scala
+++ src/main/scala/com/suprnation/MinimalTrianglePath.scala
@@ -1,12 +1,30 @@
 package com.suprnation
 
+import scala.annotation.tailrec
+
 object MinimalTrianglePath {
   type Triangle = Array[Array[Int]]
 
-  def calculate(triangle: Triangle): Seq[Int] = {
+  def calculate(triangle: Triangle): (Seq[Int], Int) = {
     require(triangle.nonEmpty, "Triangle needs to be non empty.")
-    Seq(triangle(0)(0))
+
+    @tailrec
+    def calculate(row: Int, minimalPaths: IndexedSeq[(Seq[Int], Int)]): (Seq[Int], Int) = row match {
+      case 0 => minimalPaths.head
+      case _ =>
+        val updatedMinimalPaths = triangle(row - 1).zipWithIndex.take(row).map {
+          case (node, column) =>
+            val minPath = if (minimalPaths(column)._2 <= minimalPaths(column + 1)._2) minimalPaths(column) else minimalPaths(column + 1)
+            (node +: minPath._1, minPath._2 + node)
+        }
+
+        calculate(row - 1, updatedMinimalPaths)
+    }
+
+    calculate(triangle.length - 1, toSingleNodePaths(triangle(triangle.length - 1)))
   }
 
+  private def toSingleNodePaths(row: Array[Int]) = row.map(node => (Seq(node), node))
 
 }
+
--- src/test/scala/com/suprnation/MinimalTrianglePathSpec.scala
+++ src/test/scala/com/suprnation/MinimalTrianglePathSpec.scala
@@ -13,11 +13,65 @@ class MinimalTrianglePathSpec extends WordSpec with Matchers with LoneElement {
       )
 
       val actualPath = MinimalTrianglePath.calculate(input)
-      actualPath.loneElement shouldBe onlyElement
+      actualPath._1.loneElement shouldBe onlyElement
+      actualPath._2 shouldBe onlyElement
     }
 
     "throw exception if triangle is empty" in {
       an[IllegalArgumentException] shouldBe thrownBy(MinimalTrianglePath.calculate(Array.empty))
+    }
+
+    "return the path with the smallest sum in the triangle" when {
+      "there is a unique smallest path" in {
+        val input = Array(
+          Array(7, 0, 0, 0),
+          Array(6, 3, 0, 0),
+          Array(3, 8, 5, 0),
+          Array(11, 2, 10, 9)
+        )
+
+        val actualPath = MinimalTrianglePath.calculate(input)
+        actualPath._1 should contain theSameElementsInOrderAs Seq(7, 6, 3, 2)
+        actualPath._2 shouldBe 18
+      }
+
+      "there are more minimal paths with equal sum" in {
+        val input = Array(
+          Array(7, 0, 0, 0),
+          Array(6, 2, 0, 0),
+          Array(3, 8, 5, 0),
+          Array(11, 2, 4, 9)
+        )
+        //path 7 -> 2 -> 5 -> 4 is also minimal
+        MinimalTrianglePath.calculate(input)._2 shouldBe 18
+      }
+
+      "there are duplicate minimal paths" in {
+        val input = Array(
+          Array(7, 0, 0, 0),
+          Array(6, 6, 0, 0),
+          Array(3, 3, 5, 0),
+          Array(11, 2, 10, 9)
+        )
+
+        val actualPath = MinimalTrianglePath.calculate(input)
+        actualPath._1 should contain theSameElementsInOrderAs Seq(7, 6, 3, 2)
+        actualPath._2 shouldBe 18
+      }
+
+      "there paths with negative numbers" in {
+        val input = Array(
+          Array(7, 0, 0, 0),
+          Array(6, -13, 0, 0),
+          Array(-3, 8, 5, 0),
+          Array(11, -2, 10, 9)
+        )
+
+        val actualPath = MinimalTrianglePath.calculate(input)
+        actualPath._1 should contain theSameElementsInOrderAs Seq(7, -13, 8, -2)
+        actualPath._2 shouldBe 0
+      }
+
     }
   }
 
