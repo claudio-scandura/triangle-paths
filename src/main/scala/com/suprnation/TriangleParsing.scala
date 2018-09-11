08c74c41d11c38af415625f3251101283583be9c
Claudio Scandura <cscandura@equalexperts.com>
2018-09-11 10:03:05 +0100

    TriangleParsing returns error for non numerical characters.

--- src/main/scala/com/suprnation/TriangleParsing.scala
+++ src/main/scala/com/suprnation/TriangleParsing.scala
@@ -6,6 +6,7 @@ import com.suprnation.MinimalTrianglePath.Triangle
 trait TriangleParsing {
 
   def parseTriangle(lines: List[String]): Either[String, Triangle] = {
-    Left("Triangle must contain at least a digit")
+    if (lines.isEmpty) Left("Triangle must contain at least a digit")
+    else Left("Triangle must contain only digits")
   }
 }
--- src/test/scala/com/suprnation/TriangleParsingSpec.scala
+++ src/test/scala/com/suprnation/TriangleParsingSpec.scala
@@ -4,13 +4,18 @@ import org.scalatest.{Matchers, WordSpec}
 
 class TriangleParsingSpec extends WordSpec with Matchers {
 
-  "Parsing a list of string into a triangle" should {
+  "Parsing a list of strings into a triangle" should {
 
     "return an error" when {
 
        "the list is empty" in new TriangleParsing {
          parseTriangle(List.empty) shouldBe Left("Triangle must contain at least a digit")
        }
+
+      "any of the strings contain non numerical characters" in new TriangleParsing {
+        val lines = List("23", "12 90", "1 18 foo")
+        parseTriangle(lines) shouldBe Left("Triangle must contain only digits")
+      }
     }
 
   }
