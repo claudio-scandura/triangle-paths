7ab1511a2fbf8e7552c685a86b9dbff4a68bf2a1
Claudio Scandura <cscandura@equalexperts.com>
2018-09-11 15:06:57 +0100

    TriangleParsing returns error when wrong number of digits is passed onto line and accumulates multiple errors. Returns valid triangle when valid input is passed.

--- src/main/scala/com/suprnation/TriangleParsing.scala
+++ src/main/scala/com/suprnation/TriangleParsing.scala
@@ -2,11 +2,28 @@ package com.suprnation
 
 import com.suprnation.MinimalTrianglePath.Triangle
 
+import scala.util.Try
+
 
 trait TriangleParsing {
 
-  def parseTriangle(lines: List[String]): Either[String, Triangle] = {
-    if (lines.isEmpty) Left("Triangle must contain at least a digit")
-    else Left("Triangle must contain only digits")
+  def parseTriangle(lines: List[String]): Either[String, Triangle] = lines match {
+    case Nil => Left("Triangle must contain at least a digit")
+    case _ =>
+      lines.zipWithIndex.map {
+        case (line, index) => parseLine(line, index + 1)
+      }.foldLeft[Either[String, Triangle]](Right(Array.empty[Array[Int]])) {
+        case (l@Left(_), leftOrRight) => leftOrRight.fold(error => Left(s"${l.value}, $error"), _ => l)
+        case (_, Left(error)) => Left(error)
+        case (Right(triangle), Right(parsedLine)) => Right(triangle :+ parsedLine)
+      }
+
+  }
+
+  private def parseLine(line: String, lineIndex: Int): Either[String, Array[Int]] = Try {
+    line.split(" ").map(_.toInt)
+  }.toOption.fold[Either[String, Array[Int]]](Left("Triangle must contain only digits")) {
+    case digits if digits.size != lineIndex => Left(s"Line $lineIndex of triangle must contain $lineIndex digits only")
+    case digits => Right(digits)
   }
 }
--- src/test/scala/com/suprnation/TriangleParsingSpec.scala
+++ src/test/scala/com/suprnation/TriangleParsingSpec.scala
@@ -1,20 +1,49 @@
 package com.suprnation
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{EitherValues, Matchers, WordSpec}
 
-class TriangleParsingSpec extends WordSpec with Matchers {
+class TriangleParsingSpec extends WordSpec with Matchers with EitherValues {
 
   "Parsing a list of strings into a triangle" should {
 
     "return an error" when {
 
-       "the list is empty" in new TriangleParsing {
-         parseTriangle(List.empty) shouldBe Left("Triangle must contain at least a digit")
-       }
+      "the list is empty" in new TriangleParsing {
+        parseTriangle(List.empty) shouldBe Left("Triangle must contain at least a digit")
+      }
 
-      "any of the strings contain non numerical characters" in new TriangleParsing {
+      "any of the strings contains non numerical characters" in new TriangleParsing {
         val lines = List("23", "12 90", "1 18 foo")
         parseTriangle(lines) shouldBe Left("Triangle must contain only digits")
+      }
+
+      "any of the strings contains the wrong number of digits" in new TriangleParsing {
+        val lines = List("23", "12 90", "1 18 20 11")
+        parseTriangle(lines) shouldBe Left("Line 3 of triangle must contain 3 digits only")
+      }
+    }
+
+    "accumulate errors" when {
+      "there are multiple errors" in new TriangleParsing {
+        val lines = List("23", "12 90 1", "1 18 foo")
+        val result = parseTriangle(lines)
+
+        result.left.value should (include("Triangle must contain only digits") and include("Line 2 of triangle must contain 2 digits only"))
+      }
+    }
+
+    "return a triangle" when {
+      "the string represent a valid triangle" in new TriangleParsing {
+        val lines = List("23", "12 90", "1 18 10", "99 34 6 7", "129 434 64 79 1234")
+        val result = parseTriangle(lines)
+
+        result.right.value shouldBe Array(
+          Array(23),
+          Array(12, 90),
+          Array(1, 18, 10),
+          Array(99, 34, 6, 7),
+          Array(129, 434, 64, 79, 1234)
+        )
       }
     }
 
